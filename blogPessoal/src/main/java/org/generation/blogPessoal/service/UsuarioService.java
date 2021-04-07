b4c0877cbd7370149cc4ac1c38a2004ca58dd685
Yuri <yuri.rampazo@outlook.com>
2021-04-07 13:03:21 -0300

    Corrigido Bug Usuário Duplicado

--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -17,13 +17,16 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 	
-	public Usuario CadastrarUsuario(Usuario usuario) {
-		BCryptPasswordEncoder encoder  = new BCryptPasswordEncoder();
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+		if	(repository.findByUsuario(usuario.getUsuario()).isPresent())
+			return null;
+					
+			BCryptPasswordEncoder encoder  = new BCryptPasswordEncoder();
 		
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
 		
-		return repository.save(usuario);
+		return Optional.of(repository.save(usuario));
 	}
 	
 	public Optional<UserLogin> Logar(Optional<UserLogin> user)  {
